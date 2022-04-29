cbaae068e5fc82e184d5f8720c38464abc76cf6d
cydneysmith <cydney.smith@kellogg.northwestern.edu>
2022-04-29 00:57:14 +0000

    complete

--- practice.rb
+++ practice.rb
@@ -25,15 +25,45 @@ weather_data = JSON.parse(response)
 
 # STEPS
 # Look at the weather_data hash.
+#puts weather_data
+
+
 # Find the current data.
+current_city = weather_data["region"]
+current_temperature = weather_data["currentConditions"]["temp"]["f"]
+current_conditions = weather_data["currentConditions"]["comment"]
+
 # Build a string with the text and dynamic data from the hash.
 # "In #{...} it is currently #{...} degrees and #{...}"
-# Find the array of forecast data.
-# Read only the first element of that array to display the conditions for the rest of today.
-# Use a loop to display the daily summary for the upcoming forecast.
+current_weather = "In #{current_city} it is currently #{current_temperature} degrees and #{current_conditions}"
+puts current_weather
 
-# 1. inspect the weather_data hash
-# puts weather_data
+# Find the array of forecast data.
+forecast_data = weather_data["next_days"]
+#puts forecast_data
+# Read only the first element of that array to display the conditions for the rest of today.
+today_high = forecast_data[0]["max_temp"]["f"]
+today_conditions = forecast_data[0]["comment"]
+rest_of_day_weather = "The rest of today will be a high of #{today_high} and #{today_conditions}."
+puts rest_of_day_weather
+# Use a loop to display the daily summary for the upcoming forecast.
+puts "The upcomming weather forecast is:"
+for forecast in forecast_data
+    forecast_day = forecast["day"]
+    forecast_high = forecast["max_temp"]["f"]
+    forecast_conditions = forecast["comment"]
+   puts "#{forecast_day}: a high of #{forecast_high} and #{forecast_conditions}"
+end
+
+
+
+# The upcomming weather forecast is:
+# Wednesday: a high of 65 and scattered showers.
+# Thursday: a high of 67 and partly cloudy.
+# Friday: a high of 59 and rain.
+# Saturday: a high of 77 and cloudy.
+
+
 
 # CHALLENGE
 # Can you display the weather forecast summary for a user-entered city?
